77cdfce84950be77606ab923cbeab3d4e3328a01
fourkimm <fourkimm@iabacus.co.kr>
2024-06-24 10:08:44 +0900

    Refactor: redux 연결

--- src/containers/AddNumber.jsx
+++ src/containers/AddNumber.jsx
@@ -1,18 +1,29 @@
 import AddNumber from "../components/AddNumber";
-import React, { Component } from "react";
-import store from "../store.js";
+import { connect } from "react-redux";
 
-export default class extends Component {
-  render() {
-    return (
-      <AddNumber
-        onClick={function (size) {
-          store.dispatch({
-            type: "INCREMENT",
-            size: size,
-          });
-        }.bind(this)}
-      />
-    );
-  }
+function mapReduxDispatchToReactProps(dispatch) {
+  return {
+    onClick: function (size) {
+      dispatch({
+        type: "INCREMENT",
+        size: size,
+      });
+    },
+  };
 }
+export default connect(null, mapReduxDispatchToReactProps)(AddNumber);
+
+// export default class extends Component {
+//   render() {
+//     return (
+//       <AddNumber
+//         onClick={function (size) {
+//           store.dispatch({
+//             type: "INCREMENT",
+//             size: size,
+//           });
+//         }.bind(this)}
+//       />
+//     );
+//   }
+// }
